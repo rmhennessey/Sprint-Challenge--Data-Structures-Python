966d5e39377f7c933e2f06c46457c231ba571b22
RMH <rory.hennessey@gmail.com>
2019-06-14 13:45:52 -0400

    mvp complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,18 @@
+# QUEUE Data Structure
+# FIFO (without a head and tail)
+
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
-    self.storage = [None]*capacity
+    self.storage = [None]*capacity #store the elements in the queue
 
-  def append(self, item):
-    pass
+  def append(self, item): 
+    self.storage[self.current] = item
+    if self.current != self.capacity - 1:
+      self.current += 1
+    else:
+      self.current = 0
 
   def get(self):
-    pass
+    return [i for i in self.storage if i != None]
